63bd9e7b7478b7b4e2d7923cede45c9b2d087696
Mohamed Iqbal lone <iiqballone@gmail.com>
2026-04-26 23:45:55 +0400

    user

---
+++ infra/docker/postgres-init/01-keycloak-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE surveybridge_keycloak;
+    GRANT ALL PRIVILEGES ON DATABASE surveybridge_keycloak TO $POSTGRES_USER;
+EOSQL
